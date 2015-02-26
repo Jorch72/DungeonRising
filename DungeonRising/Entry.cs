efd2d7aa727f7e49a5f4f268b0361bd6becf6f73
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-02-25 19:56:10 -0800

    Classic @ moving around the screen

--- DungeonRising/Entry.cs
+++ DungeonRising/Entry.cs
@@ -6,22 +6,112 @@ using System.Threading.Tasks;
 using BearLib;
 namespace DungeonRising
 {
+    class Entity
+    {
+        public int X, Y;
+        public char Rep;
+        public Entity(char Representation, int X, int Y)
+        {
+            this.Rep = Representation;
+            this.X = X;
+            this.Y = Y;
+        }
+        public Entity()
+        {
+            this.Rep = '.';
+            this.X = 0;
+            this.Y = 0;
+        }
+    }
+
     public class Entry
     {
+        static Entity[] MakeEntities(int startX, int startY, string reps)
+        {
+            Entity[] ret = new Entity[reps.Length];
+            for(int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = new Entity(reps[i], startX + i, startY);
+            }
+            return ret;
+        }
         public static void Run()
         {
-
+            Entity player = new Entity('@', 6, 4);
             Terminal.Open(); // terminal_open();
 
-            // Printing text
-            Terminal.Print(1, 1, "Hello, world!"); // terminal_print(1, 1, "Hello, world!");
-            Terminal.Refresh(); // terminal_refresh();
+            Entity[][] world = new Entity[8][]
+            {
+            MakeEntities(0, 0, " ###  ### "),
+            MakeEntities(0, 2, "##.####.##"),
+            MakeEntities(0, 2, "#........#"),
+            MakeEntities(0, 3, "#........#"),
+            MakeEntities(0, 4, "#........#"),
+            MakeEntities(0, 5, "#........#"),
+            MakeEntities(0, 6, "##.####.##"),
+            MakeEntities(0, 7, " ###  ### "),
+            };
+
             int input = 0; 
             // Wait until user close the window
-            while (input != Terminal.TK_CLOSE)
+            do
             {
+                switch (input)
+                {
+                    case Terminal.TK_LEFT:
+                    case Terminal.TK_KP_4:
+                    case Terminal.TK_H:
+                        {
+                            if (world[player.Y][player.X - 1].Rep == '.')
+                                player.X--;
+                        }
+                        break;
+                    case Terminal.TK_RIGHT:
+                    case Terminal.TK_KP_6:
+                    case Terminal.TK_L:
+                        {
+
+                            if (world[player.Y][player.X + 1].Rep == '.')
+                                player.X++;
+                        }
+                        break;
+                    case Terminal.TK_UP:
+                    case Terminal.TK_KP_8:
+                    case Terminal.TK_K:
+                        {
+                            if (world[player.Y - 1][player.X].Rep == '.')
+                                player.Y--;
+                        }
+                        break;
+                    case Terminal.TK_DOWN:
+                    case Terminal.TK_KP_2:
+                    case Terminal.TK_J:
+                        {
+                            if (world[player.Y + 1][player.X].Rep == '.')
+                                player.Y++;
+                        }
+                        break;
+                }
+
+                for (int y = 0; y < world.Length; y++)
+                {
+                    for (int x = 0; x < world[0].Length; x++)
+                    {
+                        if (x == player.X && y == player.Y)
+                        {
+                            Terminal.Put(x + 1, y + 1, player.Rep);
+                        }
+                        else
+                        {
+                            Terminal.Put(x + 1, y + 1, world[y][x].Rep);
+                        }
+                    }
+                }
+                Terminal.Refresh();
                 input = Terminal.Read();
-            }
+
+            } while (input != Terminal.TK_CLOSE && input != Terminal.TK_Q);
+
 
             Terminal.Close(); // terminal_close();
         }
